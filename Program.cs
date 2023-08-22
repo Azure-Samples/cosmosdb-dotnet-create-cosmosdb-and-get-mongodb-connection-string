2e0386e735f3da62fc8fc40c9d8d4e71edc36e30
Chengming <v-cruan@microsoft.com>
2023-08-22 10:39:15 +0800

    update to T2

--- Program.cs
+++ Program.cs
@@ -1,89 +1,111 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Management.Compute.Fluent;
-using Microsoft.Azure.Management.Compute.Fluent.Models;
-using Microsoft.Azure.Management.CosmosDB.Fluent;
-using Microsoft.Azure.Management.CosmosDB.Fluent.Models;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.Network.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions;
-using Microsoft.Azure.Management.Samples.Common;
-using Microsoft.Rest.Azure;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.Samples.Common;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager;
+using Azure.ResourceManager.CosmosDB.Models;
+using Azure.ResourceManager.CosmosDB;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents;
 
 namespace CosmosDBWithKindMongoDB
 {
     public class Program
     {
+        private static ResourceIdentifier? _resourceGroupId = null;
+        private const int _maxStalenessPrefix = 100000;
+        private const int _maxIntervalInSeconds = 300;
+
         /**
          * Azure CosmosDB sample -
          *  - Create a CosmosDB configured with MongoDB kind.
          *  - Get the mongodb connection string
          *  - Delete the CosmosDB.
          */
-        public static void RunSample(IAzure azure)
+        public static async Task RunSample(ArmClient client)
         {
-            string cosmosDBName = SdkContext.RandomResourceName("docDb", 10);
-            string rgName = SdkContext.RandomResourceName("rgNEMV", 24);
-
             try
             {
+                // Get default subscription
+                SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
+
+                // Create a resource group in the EastUS region
+                string rgName = Utilities.CreateRandomName("CosmosDBTemplateRG");
+                Utilities.Log($"Creating a resource group..");
+                ArmOperation<ResourceGroupResource> rgLro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(AzureLocation.EastUS));
+                ResourceGroupResource resourceGroup = rgLro.Value;
+                _resourceGroupId = resourceGroup.Id;
+                Utilities.Log("Created a resource group with name: " + resourceGroup.Data.Name);
+
                 //============================================================
                 // Create a CosmosDB.
 
-                Console.WriteLine("Creating a CosmosDB...");
-                ICosmosDBAccount cosmosDBAccount = azure.CosmosDBAccounts.Define(cosmosDBName)
-                        .WithRegion(Region.USWest)
-                        .WithNewResourceGroup(rgName)
-                        .WithKind(DatabaseAccountKind.MongoDB)
-                        .WithEventualConsistency()
-                        .WithWriteReplication(Region.USEast)
-                        .WithReadReplication(Region.USCentral)
-                        .Create();
+                Utilities.Log("Creating a CosmosDB...");
+                string dbAccountName = Utilities.CreateRandomName("dbaccount");
+                CosmosDBAccountKind cosmosDBKind = CosmosDBAccountKind.MongoDB;
+                var locations = new List<CosmosDBAccountLocation>()
+                {
+                    new CosmosDBAccountLocation(){ LocationName  = AzureLocation.WestUS, FailoverPriority = 0 },
+                };
+                var dbAccountInput = new CosmosDBAccountCreateOrUpdateContent(AzureLocation.WestUS2, locations)
+                {
+                    Kind = cosmosDBKind,
+                    ConsistencyPolicy = new Azure.ResourceManager.CosmosDB.Models.ConsistencyPolicy(DefaultConsistencyLevel.BoundedStaleness)
+                    {
+                        MaxStalenessPrefix = _maxStalenessPrefix,
+                        MaxIntervalInSeconds = _maxIntervalInSeconds
+                    },
+                    IsVirtualNetworkFilterEnabled = true,
+                    EnableAutomaticFailover = false,
+                    ConnectorOffer = ConnectorOffer.Small,
+                    DisableKeyBasedMetadataWriteAccess = false,
+                    EnableMultipleWriteLocations = true,
+                    PublicNetworkAccess = CosmosDBPublicNetworkAccess.Enabled,
+                };
 
-                Console.WriteLine("Created CosmosDB");
-                Utilities.Print(cosmosDBAccount);
+                dbAccountInput.Tags.Add("key1", "value");
+                dbAccountInput.Tags.Add("key2", "value");
+                var accountLro = await resourceGroup.GetCosmosDBAccounts().CreateOrUpdateAsync(WaitUntil.Completed, dbAccountName, dbAccountInput);
+                CosmosDBAccountResource dbAccount = accountLro.Value;
+                Utilities.Log($"Created CosmosDB {dbAccount.Id.Name}");
 
                 //============================================================
                 // Get credentials for the CosmosDB.
 
-                Console.WriteLine("Get credentials for the CosmosDB");
-                var databaseAccountListKeysResult = cosmosDBAccount.ListKeys();
-                string masterKey = databaseAccountListKeysResult.PrimaryMasterKey;
-                string endPoint = cosmosDBAccount.DocumentEndpoint;
+                Utilities.Log("Get credentials for the CosmosDB..");
+                var getKeysLro = await dbAccount.GetKeysAsync();
+                CosmosDBAccountKeyList keyList = getKeysLro.Value;
+                string masterKey = keyList.PrimaryMasterKey;
+                string endPoint = dbAccount.Data.DocumentEndpoint;
+                Utilities.Log($"masterKey: {masterKey}");
+                Utilities.Log($"endPoint: {endPoint}");
 
-                Console.WriteLine("Get the MongoDB connection string");
-                var databaseAccountListConnectionStringsResult = cosmosDBAccount.ListConnectionStrings();
-                Console.WriteLine("MongoDB connection string: "
-                        + databaseAccountListConnectionStringsResult.ConnectionStrings[0].ConnectionString);
+                Utilities.Log("Get the MongoDB connection string..");
+                var connStringLro = await dbAccount.GetConnectionStringsAsync().ToEnumerableAsync();
+                Utilities.Log($"{connStringLro[0].Description}: {connStringLro[0].ConnectionString}");
 
                 //============================================================
                 // Delete CosmosDB
-                Console.WriteLine("Deleting the CosmosDB");
-                // work around CosmosDB service issue returning 404 CloudException on delete operation
-                try
-                {
-                    azure.CosmosDBAccounts.DeleteById(cosmosDBAccount.Id);
-                }
-                catch (CloudException)
-                {
-                }
-                Console.WriteLine("Deleted the CosmosDB");
+                Utilities.Log("Deleting the CosmosDB");
+                await dbAccount.DeleteAsync(WaitUntil.Completed);
+
+                Utilities.Log($"Deleted the CosmosDB {dbAccount.Data.Name}");
             }
             finally
             {
                 try
                 {
-                    Utilities.Log("Deleting resource group: " + rgName);
-                    azure.ResourceGroups.BeginDeleteByName(rgName);
-                    Utilities.Log("Deleted resource group: " + rgName);
+                    if (_resourceGroupId is not null)
+                    {
+                        Utilities.Log($"Deleting Resource Group..");
+                        await client.GetResourceGroupResource(_resourceGroupId).DeleteAsync(WaitUntil.Completed);
+                        Utilities.Log($"Deleted Resource Group: {_resourceGroupId}");
+                    }
                 }
                 catch (NullReferenceException)
                 {
@@ -96,24 +118,20 @@ namespace CosmosDBWithKindMongoDB
             }
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 //=================================================================
                 // Authenticate
-                var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+                var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+                var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                var subscription = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+                ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                ArmClient client = new ArmClient(credential, subscription);
 
-                var azure = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials)
-                    .WithDefaultSubscription();
-
-                // Print selected subscription
-                Utilities.Log("Selected subscription: " + azure.SubscriptionId);
-
-                RunSample(azure);
+                await RunSample(client);
             }
             catch (Exception e)
             {
